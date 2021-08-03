a9cf8e07a3d72bb91207620a351666c5c4ed8cd5
Tristan Call <tristanc@minds02.isi.edu>
2021-08-03 10:16:18 -0700

    Putting correct code in the right place

--- run_all_network.py
+++ run_all_network.py
@@ -1,15 +1,15 @@
-import construct_yearly_networks
+import sys
 import subprocess
 
 # Quick and dirty script to run construct network over a set number of years
-# call with python3 workflow/run-all-network 2000
+# call with python3 workflow/run_all_network.py 2000 2001 
 
 if __name__ == "__main__":
     YEARS = [int(y) for y in sys.argv[1:]]
 
     for year in YEARS:
-        subprocess.Popen(['python3', 'workflow/construct_yearly_networks.py', 'data/networks/paper_count_journals.csv', 'data/journal_networks', '2011', '2', 'data/journal_networks/nodes-2011.csv', 'data/journal_networks/edges-2011.csv'])
-        subprocess.Popen(['python3', 'workflow/construct_yearly_networks.py', 'data/networks/paper_count_journals.csv', 'data/journal_networks', '2011', '9999', 'data/journal_networks/raw-nodes-2011.csv', 'data/journal_networks/raw-edges-2011.csv'])
+        subprocess.Popen(['python3', 'workflow/construct_yearly_networks.py', 'data/journal_networks/paper_count.csv', 'data/journal_networks', str(year), '2', 'data/journal_networks/nodes-{year}.csv'.format(year=year), 'data/journal_networks/edges-{year}.csv'.format(year=year)])
+        subprocess.Popen(['python3', 'workflow/construct_yearly_networks.py', 'data/journal_networks/paper_count.csv', 'data/journal_networks', str(year), '9999', 'data/journal_networks/raw-nodes-{year}.csv'.format(year=year), 'data/journal_networks/raw-edges-{year}.csv'.format(year=year)])
 
 
 
